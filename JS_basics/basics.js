b3acd3e7d75e99b447554193cec899c54ffae258
Ian Iversen <ianiversen1@gmail.com>
2022-11-08 18:15:33 -0500

    objects started

--- JS_basics/basics.js
+++ JS_basics/basics.js
@@ -1,4 +1,40 @@
-let num = 10.5;
+let str = 'abcd';
 
-console.log(5)
-console.log(typeo);
+console.log(str === 'abcd');
+
+console.log('Hello this is a single quote: \'');
+
+console.log('Hello\nWorld');
+
+console.log(`Hello
+World`);
+
+console.log(`Thirty: ${10 + 20}`);
+
+
+console.log('This is a very ' +
+            'long string');
+
+console.log('this is a very \
+long string')
+
+str = 'abcd';
+
+console.log(str.includes('a'));
+
+console.log(str.endsWith('d'));
+
+console.log(str.toUpperCase());
+
+
+console.log(str.substring(0, 2));
+
+console.log(str.slice(1, 3));
+
+console.log(str.padStart(10, '-'));
+
+console.log(str.trim()); //this comes in handy if 'str' had a bunch of spaces like '       abcd'
+
+let ian = 'a,b,c,d';
+
+console.log(ian.split(','));
