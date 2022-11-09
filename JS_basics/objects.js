8614af8b8541966228b4b49c915887a0e2adc4ac
Ian Iversen <ianiversen1@gmail.com>
2022-11-09 10:07:12 -0500

    error handling started

--- JS_basics/objects.js
+++ JS_basics/objects.js
@@ -1,16 +1,126 @@
-const person = {
-    name: 'Conner',
-    course: 'FrontendExpert',
-};
+// const person = {
+//     name: 'Conner',
+//     course: 'FrontendExpert',
+// };
 
-console.log(person);
+// console.log(person);
 
-console.log(JSON.parse(JSON.stringify(person)));
+// console.log(JSON.parse(JSON.stringify(person)));
 
-console.log(typeof JSON.stringify(person));
+// console.log(typeof JSON.stringify(person));
 
 
-person.name = 'Clement';
-console.log(person.name);
+// person.name = 'Clement';
+// console.log(person.name);
 
-const map = new Map();
+// const map = new Map();
+
+// map.set(123, 'hello');
+// map.set(123, 'world');
+
+// console.log(map.get(123));
+
+// const set = new Set();
+// set.add('hello');
+// set.add('world');
+// set.delete('world');
+// console.log(set.has('hello'));
+// console.log(set.size);
+
+// const arr = [1, 2, 3];
+
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr[1])
+// console.log(typeof arr)
+
+// function addTwo(num = 6) {
+//     return num + 2;
+// }
+
+// function callFunc(func, param) {
+//     console.log(func(param));
+// }
+
+// console.log(addTwo(10));
+
+
+// console.log(addTwo instanceof Object);
+
+
+// for (let i = 0; i < 4; i++) {
+
+//     if (i === 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// let i = 0;
+// while (i < 4) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// }
+// while (i < 0)
+
+// for (const value of [1, 2, 3]) {
+//     console.log(value);
+// }
+
+// for (const value of 'abc') {
+//     console.log(value);
+// }
+
+// const obj = {
+//     name: 'Conner',
+//     course: 'FrontendExpert',
+// };
+
+// for (const key in obj) {
+//     console.log(key, obj[key]);
+// }
+
+// [1, 2, 3].forEach(function(value) {
+//     console.log(value);
+// })
+
+//anonymous function^^
+
+
+// const condition = null;
+
+// if (condition) {
+//     console.log('it was true');
+// } else if (condition === false) {
+//     console.log('it was false');
+// } else {
+//     console.log('Default');
+// }
+
+
+// const myNum = 2;
+// switch (myNum) {
+//     case 1:
+//         console.log('It was 1');
+//         break;
+//     case 2:
+//         console.log('It was 2');
+//         break;
+//     default:
+//         console.log('Default');
+// }
+
+// const myNum = 10;
+
+// console.log(myNum > 5 ? 'Greater than 5' : 'Less than 5');
+
+// throw new Error('Oh No');
+
+throw Error
