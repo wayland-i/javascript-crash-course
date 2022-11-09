dc215c81f5208559b9d938ec5280575f296fb963
Ian Iversen <ianiversen1@gmail.com>
2022-11-09 12:36:33 -0500

    variables done

---
+++ variables.js
@@ -0,0 +1,34 @@
+
+
+// function test() {
+//     const constNum = 0;
+//     if (true) {
+//         var varNum = 0;
+        
+//     }
+    
+//     console.log('varNum', varNum);
+    
+//     }
+
+// test();
+
+
+function test() {
+    const constNum = 0;
+    let letNum = 0;
+    const arr = [1, 2, 3];
+
+    arr.push(4);
+    console.log(arr);
+
+    // letNum = 2;
+    // constNum = 2;
+
+    // console.log('letNum', letNum);
+    // console.log('constNum', letNum);
+}
+
+test();
+
+
