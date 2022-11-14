e8215e486cb76450271b76268a0d31c6ad0d93af
Ian Iversen <ianiversen1@gmail.com>
2022-11-13 21:31:53 -0500

    deleting and replacing

--- arrays.js
+++ arrays.js
@@ -18,10 +18,34 @@
 // console.log(arr.lastIndexOf(3));
 // console.log(arr);
 
+/*
+
+            break
+
+*/
+
+
+// const arr = [1, 2, 3];
+
+// arr.push(4);
+// arr.pop();
+// arr.pop();
+
+// arr.push(4, 5, 6);
+
+// arr.unshift(4, 5, 6);
+
+// console.log(arr.shift());
+
+// console.log(typeof arr);
+
+// console.log(Array.isArray(arr));
+// console.log(arr instanceof Array);
+
+// console.log(arr);
+
+////break///////
+
 const arr = [1, 2, 3];
 
-arr.push(4);
-arr.pop();
-arr.pop();
-
-console.log(arr)
+arr.splice();
