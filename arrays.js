2332048ea104187c3bc31b3beca38f7f43419dfb
Ian Iversen <ianiversen1@gmail.com>
2022-11-13 21:19:28 -0500

    arrays started

---
+++ arrays.js
@@ -0,0 +1,27 @@
+// const arr = [1, 2, 3];
+
+// const arr = new Array(5).fill(0);
+
+// const arr = new Array();
+// arr.length = 10;
+// arr.fill(0);
+
+// console.log(arr)
+
+
+// const arr = [1, 2, 3, 3];
+
+// arr[1] = 10;
+
+// console.log(arr.includes(3));
+// console.log(arr. indexOf(3));
+// console.log(arr.lastIndexOf(3));
+// console.log(arr);
+
+const arr = [1, 2, 3];
+
+arr.push(4);
+arr.pop();
+arr.pop();
+
+console.log(arr)
