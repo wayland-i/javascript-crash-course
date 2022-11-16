3baf927bcb53d179d4ec00a91ecbfaa0c4b6580f
Ian Iversen <ianiversen1@gmail.com>
2022-11-16 17:33:04 -0500

    destructuring objects

--- syntacticSugar.js
+++ syntacticSugar.js
@@ -1,9 +1,37 @@
-const arr = [1, 2, 3, 4];
+// const arr = [1, 2, 3, 4];
 
-const doubled = arr.map(num => num * 2);
+// const doubled = arr.map(num => num * 2);
 
-function double(num) {
-    return num * 2;
+// function double(num) {
+//     return num * 2;
+// };
+
+// console.log(doubled)
+
+// const arr = [1, 2, 3, 4];
+
+// const [first, second, ...rest] = arr;
+
+// const first = arr[0];
+// const second = arr[1];
+
+// console.log(first, second);
+// console.log(rest)
+// console.log(arr);
+
+
+const person = {
+    name: 'Conner',
+    website: 'FrontendExpert'
 };
 
-console.log(doubled)
+// const { name: firstName, company='AlgoExpert', ...rest } = person;
+// console.log(firstName);
+// console.log(company);
+// console.log(rest);
+
+function printName({name}) {
+    console.log(name);
+};
+
+printName(person);
