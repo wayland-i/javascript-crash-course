3eff2831d2074045f542d463a533e64130ac06ef
Ian Iversen <ianiversen1@gmail.com>
2022-11-16 11:39:17 -0500

    object equality started

--- objects.js
+++ objects.js
@@ -11,10 +11,36 @@
 // console.log(website)
 
 
+// const website = {
+//     name: 'AlgoExpert',
+//     rating: 5,
+//     founders: ['Clement', 'Antoine'],
+// };
+
+// const key = 'name'
+
+// console.log(website[key])
+
+
+// const website = {
+//     name: 'AlgoExpert',
+//     rating: 5,
+//     founders: ['Clement', 'Antoine'],
+// };
+
+// website.name = 'FrontendExpert';
+// console.log(website.name)
+
+// website.foo = 'bar';
+// console.log(website)
+
+// delete website.foo;
+// console.log(website);
+
 const website = {
     name: 'AlgoExpert',
     rating: 5,
     founders: ['Clement', 'Antoine'],
 };
 
-console.log(website)
+console.log()
