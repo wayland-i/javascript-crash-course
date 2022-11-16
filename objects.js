1d4275f8c42132b19272b12acb9f4ed1c57fc632
Ian Iversen <ianiversen1@gmail.com>
2022-11-16 11:09:10 -0500

    objects started

---
+++ objects.js
@@ -0,0 +1,20 @@
+// const myKey = 'key';
+// const website = {
+//     name: 'AlgoExpert',
+//     rating: 5,
+//     founders: ['Clement', 'Antoine'],
+//     isAwesome: true,
+//     'multi word key': 0,
+//     [myKey]: 1234
+// };
+
+// console.log(website)
+
+
+const website = {
+    name: 'AlgoExpert',
+    rating: 5,
+    founders: ['Clement', 'Antoine'],
+};
+
+console.log(website)
