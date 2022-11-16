2f1fbbce5f13ba8ce8f67a8d3e0cc9a4e1eb680f
Ian Iversen <ianiversen1@gmail.com>
2022-11-16 16:08:55 -0500

    NaN != NaN

---
+++ equality.js
@@ -0,0 +1,19 @@
+console.log(5 == '5'); // "loose" equality, values
+console.log(5 === '5'); // "strict" equality, types and values
+
+// console.log(Number(false));
+// console.log(Number(true));
+// console.log(false == 0);
+// console.log(true == 1);
+
+// console.log(Boolean(0));
+
+// console.log(String(0));
+
+// console.log(typeof(0));
+
+// console.log(Number('abc'));
+
+// console.log(NaN == NaN);
+
+const x = 5;
