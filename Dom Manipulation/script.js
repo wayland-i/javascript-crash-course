4cace7f367949ad8d0ca3c4d23ff44b33bc1e880
Ian Iversen <ianiversen1@gmail.com>
2022-11-17 13:07:10 -0500

    moving on from node issue

--- Dom Manipulation/script.js
+++ Dom Manipulation/script.js
@@ -1,10 +1,11 @@
+
+console.log(document.getElementById('second-li'));
+
 const listItems = document.querySelectorAll('li');
 console.log(listItems);
-
-console.log(document.querySelector('p'))
-console.log(document.querySelector('ol'))
 
 
 listItems.forEach(li => {
     console.log(li);
 });
+
