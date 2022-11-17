dff51bb4a95ef137b1f06300377f4542eef67ef9
Ian Iversen <ianiversen1@gmail.com>
2022-11-17 17:44:47 -0500

    common events

--- Event-Driven Programming/script.js
+++ Event-Driven Programming/script.js
@@ -1,11 +1,27 @@
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-button.addEventListener('click', onClick);
+// const abortController = new AbortController();
 
-document.body.addEventListener('click', onClick);
+// button.addEventListener('click', onClick, {
+//     capture: true,
+//     once: true,
+//     passive: true,
+//     signal: abortController.signal
+// });
 
-function onClick(event) {
-    console.log(event.target);
-    console.log(this)
-    console.log('You clicked the button');
-};
+// abortController.abort();
+
+// document.body.addEventListener('click', onClick, true);
+
+// // button.removeEventListener('click', onClick);
+
+// button.addEventListener('dblclick', onClick);
+// // 'mousedown', 'mouseup'
+
+// function onClick(event) {
+//     // event.preventDefault();
+//     console.log(event.target);
+//     console.log(this)
+//     console.log('You clicked the button');
+// };
+
