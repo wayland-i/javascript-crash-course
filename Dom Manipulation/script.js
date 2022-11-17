651c693834f4f2ab96732b1fb74cd1d88b25285d
Ian Iversen <ianiversen1@gmail.com>
2022-11-17 11:40:58 -0500

    node selection issue

---
+++ Dom Manipulation/script.js
@@ -0,0 +1,10 @@
+const listItems = document.querySelectorAll('li');
+console.log(listItems);
+
+console.log(document.querySelector('p'))
+console.log(document.querySelector('ol'))
+
+
+listItems.forEach(li => {
+    console.log(li);
+});
