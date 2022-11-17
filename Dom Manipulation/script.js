bf02d2f525f544ba43a2fdc53cb78e3524127fd7
Ian Iversen <ianiversen1@gmail.com>
2022-11-17 14:11:07 -0500

    for loop working

--- Dom Manipulation/script.js
+++ Dom Manipulation/script.js
@@ -1,5 +1,26 @@
-const secondLi = document.getElementById('second-li');
-console.log(secondLi);
+// const listItems = document.querySelectorAll('li');
+
+// listItems[0].style.backgroundColor = 'red';
+
+// listItems[0].textContent = 'Changed!';
+
+// // listItems[0].setAttribute('class', 'blue big');
+// listItems[0].classList.add('blue');
+// listItems[0].classList.toggle('big')
+// listItems[0].classList.toggle('big')
+// listItems[0].className = 'blue'
+
 
 const listItems = document.querySelectorAll('li');
-console.log(listItems);
+
+const p = document.createElement('p');
+p.textContent = 'Hello World';
+
+document.body.appendChild(p);
+
+for (let i = 0; i < 3; i++) {
+    const parent = document.querySelector('ol');
+    const li = document.createElement('li');
+    li.textContent = `List item with i=${i}`;
+    parent.append(li)
+}
