585cd7d2b4ee7b7462c0ec755f9eb0600456ff29
Ian Iversen <ianiversen1@gmail.com>
2022-11-18 11:22:55 -0500

    .finally() started

--- promises.js
+++ promises.js
@@ -8,13 +8,41 @@
 // console.log(promise) 
 
 
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve(2), 1000);
-    // setTimeout(() => reject(new Error('Something went wrong')), 1000)
-});
+// const promise = new Promise((resolve, reject) => {
+//     // setTimeout(() => resolve(2), 1000);
+//     setTimeout(() => reject(new Error('Something went wrong')), 1000)
+// });
+
+
+// console.log(promise);
+
+// // promise.then(value => console.log(value), error => console.log('On no ' + error));
+
+// promise.then(console.log).catch(error => console.log('On No ' + error));
+
+// const promise = new Promise((resolve, reject) => {
+//     // setTimeout(() => resolve(2), 1000);
+//     setTimeout(() => reject(new Error('Something went wrong')), 1000)
+// });
+
+const promise = Promise.resolve(3);
 
 console.log(promise);
 
 // promise.then(value => console.log(value), error => console.log('On no ' + error));
 
-promise.then(console.log).catch(error => console.log('On No ' + error));
+// promise.then(console.log).catch(error => console.log('On No ' + error));
+
+promise
+    .then(value => value * 2)
+    .then(value => value + 1)
+    .then(value => {
+        throw new Error('Something went wrong');
+    })
+    .then(console.log)
+    .catch(error => {
+        console.log('Oh No ' + error);
+        // return 10;
+    })
+    .then(console.log);
+        
