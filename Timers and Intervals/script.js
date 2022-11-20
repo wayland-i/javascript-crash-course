2e1f459fadc071e0c7d29821ec6e3ca1d8eaaa41
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 12:51:46 -0500

    timers and intervals started

---
+++ Timers and Intervals/script.js
@@ -0,0 +1,15 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const count = document.getElementById('count');
+
+start.addEventListener('click', startTime);
+stop.addEventListener('click', stopTIme);
+
+
+function startTime() {
+
+}
+
+function stopTIme() {
+    
+}
