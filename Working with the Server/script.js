271d9bcb9cfb963a22734b885cd12b15443823ac
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 11:58:34 -0500

    json server running

--- Working with the Server/script.js
+++ Working with the Server/script.js
@@ -1,5 +1,6 @@
-const BASE_API = 'http://localhost:8080/api';
-
+const BASE_API = 'http://localhost:3000/posts';
+const JSON_API = 'http://localhost:3000/comments';
+const POST_API = 'http://localhost:3000/profile';
 
 
 fetch(BASE_API).then(console.log);
