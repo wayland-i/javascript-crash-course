9d7ae031804fbaf51b4b60718e7bad992e77c7e5
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 15:07:59 -0500

    timers and intervals done

--- Timers and Intervals/script.js
+++ Timers and Intervals/script.js
@@ -1,5 +1,5 @@
-const start = document.getElementById('start');
 const stop = document.getElementById('stop');
+const start = document.getElementById('start');
 const count = document.getElementById('count');
 
 start.addEventListener('click', startTime);
@@ -11,14 +11,36 @@ let timerID;
 //     console.log('timeout');
 // }, 1000);
 
+
 // clearTimeout(timeoutID);
 
-function startTime() {
-    timerID = setInterval(() => {
-        count.textContent++;
-    }, 500);
+let animationFrameID;
+
+
+// setTimeout(() => {
+//     console.log(performance.now());
+// }, 1000);
+
+
+// const date = new Date(2025, 0, 10, 9, 25, 10, 30);
+// console.log(date)
+
+const date = new Date(2025, 0, 10, 9, 25, 10, 30);
+console.log(date.getMonth())
+console.log(date.getDay())
+date.setMonth(9);
+console.log(date)
+
+function startTime(timestamp) {
+    console.log(timestamp);
+    // timerID = setInterval(() => {
+    //     count.textContent++;
+    // }, 500);
+    count.textContent++;
+    animationFrameID = requestAnimationFrame(startTime);
 }
 
 function stopTime() {
-    clearInterval(timerID);
+    // clearInterval(timerID);
+    cancelAnimationFrame(animationFrameID)
 }
