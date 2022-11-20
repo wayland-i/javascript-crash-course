5a86235244813b2dab7e47d8d9457feb77177b52
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 12:20:50 -0500

    jsonAPI section started

--- Working with the Server/script.js
+++ Working with the Server/script.js
@@ -1,6 +1,27 @@
 const BASE_API = 'http://localhost:3000/posts';
 const JSON_API = 'http://localhost:3000/comments';
 const POST_API = 'http://localhost:3000/profile';
+const BROKEN_API = 'http://localhost:3000/broken';
+
+// fetch(BASE_API).then(res => res.text()).then(console.log)
+
+// fetch(BASE_API)
+//     .then(res => res)
+//     .then(console.log)
+//     .catch(error => console.error('Oh No ' + error));
+
+async function main() {
+    try {
+        const response = await fetch(BASE_API);
+        console.log(response.status);
+        console.log(response.ok)
+        const text = await response.text();
+        console.log(text);
+    } catch (error) {
+        console.log('Oh No: ' + error)
+    }
+}
 
 
-fetch(BASE_API).then(console.log);
+main()
+
