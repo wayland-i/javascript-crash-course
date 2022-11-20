e641d05b78a51c3ece63196222dad23cdcc6837c
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 15:31:06 -0500

    scope and variable assignment  in functions

--- closures.js
+++ closures.js
@@ -1,10 +1,38 @@
-let globalNum = 5;
+// let globalNum = 5;
 
-function logNum() {
-    const localNum = 1;
-    console.log(globalNum + localNum);
-}
+// function logNum() {
+//     const localNum = 1;
+//     console.log(globalNum + localNum);
+// }
 
-// globalNum = 10;
-logNum();
-console.log('hello')
+// // globalNum = 10;
+// logNum();
+// console.log('hello')
+
+// function example() {
+//     const num = 5;
+
+//     return function logNum() {
+//         console.log(num)
+//     }
+// }
+
+// const innerFunction = example();
+
+// innerFunction();
+
+// function makeFunctions() {
+//     let privateNum = 0;
+
+//     function privateIncrement() {
+//         privateNum++;
+//     }
+
+//     return {
+//         logNum: () => console.log(privateNum),
+//         increment: () => {
+//             privateIncrement();
+//             console.log('Incremented!');
+//         }
+//     }
+// }
