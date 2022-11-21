576cdd832eae0c77aacd88d3341992b799c61621
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 19:34:10 -0500

    .prototypeOf is helpful

--- classes.js
+++ classes.js
@@ -13,3 +13,20 @@
 
 // console.log(person.maxAge);
 // console.log(child.maxAge);
+
+// const person = { 
+//     isHuman: true
+// };
+
+// const child = Object.create(person);
+// child.maxAge = 18;
+
+// const john = Object.create(child);
+// john.name = 'John';
+
+// child.maxAge = 19;
+
+// console.log(john.maxAge);
+
+// const arrayProto = Object.getPrototypeOf([]);
+// console.log(Object.getOwnPropertyNames(arrayProto));
