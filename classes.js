ed5f14fc0f110f28ef959955be2c443fdf1a6ed4
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 19:58:10 -0500

    function constructors done

--- classes.js
+++ classes.js
@@ -30,3 +30,28 @@
 
 // const arrayProto = Object.getPrototypeOf([]);
 // console.log(Object.getOwnPropertyNames(arrayProto));
+
+function Person(name) {
+    this.name = name
+}
+
+Person.prototype = {
+    constructor: Person,
+    isHuman: true
+};
+
+const conner = new Person('Conner');
+const ian = new Person('Ian');
+console.log(conner);
+console.log(ian);
+
+
+Person.prototype.speak = function() {
+    console.log("hello this is " + this.name);
+}
+
+ian.speak();
+
+console.log(conner instanceof Person);
+console.log(ian instanceof Object);
+
