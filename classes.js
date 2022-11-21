c9e901767d9eebf124ce0ac1ebc9bc35b7190a94
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 20:14:17 -0500

    class inheritance started

--- classes.js
+++ classes.js
@@ -31,27 +31,68 @@
 // const arrayProto = Object.getPrototypeOf([]);
 // console.log(Object.getOwnPropertyNames(arrayProto));
 
-function Person(name) {
-    this.name = name
+// function Person(name) {
+//     this.name = name
+// }
+
+// Person.prototype = {
+//     constructor: Person,
+//     isHuman: true
+// };
+
+// const conner = new Person('Conner');
+// const ian = new Person('Ian');
+// console.log(conner);
+// console.log(ian);
+
+
+// Person.prototype.speak = function() {
+//     console.log("hello this is " + this.name);
+// }
+
+// ian.speak();
+
+// console.log(conner instanceof Person);
+// console.log(ian instanceof Object);
+
+
+// This is a Polyfill
+
+// Array.prototype.myPush = function(value) {
+//     this[this.length] = value;
+// }
+
+// const arr = [1, 2, 3];
+// arr.myPush(4);
+// console.log(arr);
+
+// class Person {
+//     static isHuman = true;
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     speak() {
+//         console.log('Hello this is ' + this.name);
+//     }
+//     static greet() {
+//         console.log('hello');
+//     }
+// }
+
+// const conner = new Person('Conner');
+// const clement = new Person('Clement');
+// conner.speak();
+// // conner.isHuman = false;
+// console.log(conner.isHuman);
+// console.log(clement.isHuman);
+// console.log(Person.isHuman);
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    speak() {
+        console.log('Hello this is ' + this.name);
+    }
 }
-
-Person.prototype = {
-    constructor: Person,
-    isHuman: true
-};
-
-const conner = new Person('Conner');
-const ian = new Person('Ian');
-console.log(conner);
-console.log(ian);
-
-
-Person.prototype.speak = function() {
-    console.log("hello this is " + this.name);
-}
-
-ian.speak();
-
-console.log(conner instanceof Person);
-console.log(ian instanceof Object);
 
