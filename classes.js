96820f0f750e4e4a4dffea0e987575e009212709
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 19:21:15 -0500

    7 mins in

---
+++ classes.js
@@ -0,0 +1,15 @@
+// const person = {
+//     isHuman: true
+// };
+
+// // const child = Object.assign(Object.create(person), {
+// //     maxAge: 18
+// // });
+
+// const child = {
+//     maxAge: 18,
+//     __proto__: person
+// };
+
+// console.log(person.maxAge);
+// console.log(child.maxAge);
