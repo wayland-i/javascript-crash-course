da6f61721217a8ea87027a9fa259c992de7800e6
Ian Iversen <ianiversen1@gmail.com>
2022-11-21 13:59:24 -0500

    end of cookies section

--- Browser Storage/script.js
+++ Browser Storage/script.js
@@ -1,3 +1,11 @@
-document.cookie = `user=Clement; secure; samesite=strict`;
-document.cookie = 'course=FrontendExpert';
-console.log(document.cookie);
+// document.cookie = `user=Clement; secure; samesite=strict`;
+// document.cookie = 'course=FrontendExpert';
+// console.log(document.cookie);
+
+const course = document
+    .cookie
+    .split('; ')
+    .find(cookie => cookie.startsWith('course='))
+    .split('=')[1];
+
+console.log(course)
