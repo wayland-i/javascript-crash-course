6b2f3669bb3bafebb9586f8082fda89b3171abeb
Ian Iversen <ianiversen1@gmail.com>
2022-11-21 17:08:55 -0500

    datastructures started

---
+++ dataStructures.js
@@ -0,0 +1,30 @@
+// const stack = [];
+
+// stack.push(1);
+// stack.push(2);
+
+// console.log(stack.pop());
+
+// const queue = [];
+// queue.push(1);
+// queue.push(2);
+// console.log(queue.shift());
+
+// const map = new Map();
+
+// map.set('test', 123);
+// map.set(10, 'ten');
+// // map.delete(10)
+// // map.clear()
+
+
+// console.log(map.get(10));
+// console.log(map.has(10));
+// console.log(map.has('ten'));
+// console.log(map.size);
+
+
+const map = new Map();
+
+map.set('test', 123);
+map.set(10, 'ten');
