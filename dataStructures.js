a4b48a3550064883cce03a98fd43f6e24cda5b94
Ian Iversen <ianiversen1@gmail.com>
2022-11-21 17:39:40 -0500

    datastructures done

--- dataStructures.js
+++ dataStructures.js
@@ -60,3 +60,43 @@
 // })();
 
 
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    addStart(value) {
+        const node = new Node(value);
+        const tempHead = this.head;
+        this.head = node;
+        node.next = tempHead;
+    }
+
+    addEnd(value) {
+        const node = new Node(value);
+        let curr = this.head;
+
+        if (curr == null) {
+            this.head = node;
+            return;
+        }
+
+        while (curr != null && curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = node;
+    }
+}
+
+const list = new LinkedList();
+list.addStart(1);
+list.addStart(2);
+list.addEnd(3);
+console.log(list.head.next.next.value)
