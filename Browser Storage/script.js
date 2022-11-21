79401d1de07b6b67182eb75b59289ae90d1dff14
Ian Iversen <ianiversen1@gmail.com>
2022-11-21 14:25:16 -0500

    indexedDB example

--- Browser Storage/script.js
+++ Browser Storage/script.js
@@ -14,9 +14,29 @@
 
 //session storage expires, local storage has to be deleted manually
 
-localStorage.setItem('user', 'Ian');
-localStorage.setItem('course', 'AlgoExpert');
-console.log(localStorage.getItem('user'));
-// localStorage.removeItem('user');
-// localStorage.clear();
-console.log(localStorage.getItem('user'));
+// localStorage.setItem('user', 'Ian');
+// localStorage.setItem('course', 'AlgoExpert');
+// console.log(localStorage.getItem('user'));
+// // localStorage.removeItem('user');
+// // localStorage.clear();
+// console.log(localStorage.getItem('user'));
+
+
+const request = indexedDB.open('myDatabase', 1);
+
+request.addEventListener('upgradeneeded', event => {
+    const database = event.target.result;
+    const store = database.createObjectStore('users', {keyPath: 'id'});
+    store.createIndex('name', 'name');
+
+    store.add({
+        id: 0,
+        name: 'Conner',
+        course: 'FrontendExpert'
+    });
+    store.add({
+        id: 1,
+        name: 'Clement',
+        course: 'AlgoExpert'
+    });
+})
