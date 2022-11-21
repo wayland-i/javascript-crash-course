b8f190959b6ba1bce62145e10cb8883fa6db5785
Ian Iversen <ianiversen1@gmail.com>
2022-11-20 22:42:22 -0500

    generators done

--- generators.js
+++ generators.js
@@ -1,11 +1,25 @@
-function* genNumbers() {
-    yield 1;
-    yield 2;
-    yield 3;
-    return 4;
+// function* genNumbers() {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+//     return 4;
+// }
+
+// const generator = genNumbers();
+
+
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+// console.log(generator.next());
+
+function* genNumbers(count) {
+    for (let i=0; i < count; i++) {
+        yield i;
+    }
 }
 
-const generator = genNumbers();
+const generator = genNumbers(3);
 
 
 console.log(generator.next());
