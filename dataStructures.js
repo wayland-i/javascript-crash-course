a7b81857c5b0d28bec5682b0f664b6cc218e7239
Ian Iversen <ianiversen1@gmail.com>
2022-11-21 17:31:41 -0500

    linked lists started

--- dataStructures.js
+++ dataStructures.js
@@ -24,7 +24,39 @@
 // console.log(map.size);
 
 
-const map = new Map();
+// const map = new Map();
 
-map.set('test', 123);
-map.set(10, 'ten');
+// map.set('test', 123);
+// map.set(10, 'ten');
+
+// for ([key, value] of map) {
+//     console.log(key, value);
+// }
+
+// map.forEach((value, key) => {
+//     console.log(key, value);
+// }) 
+
+// const set = new Set();
+// set.add(123);
+// set.add(456);
+
+// for (value of set) {
+//     console.log(value);
+// }
+
+// set.forEach(value => {
+//     console.log(value);
+// })
+
+// const arr =  [1, 2, 2, 3, 4];
+// console.log(Array.from(new Set(arr)));
+
+// const weakSet = new WeakSet();
+
+// (function() {
+//     const obj = {};
+//     weakSet.add(obj);
+// })();
+
+
