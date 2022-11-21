6390a9c78fa5a68e34105da3125a50e968858eae
Ian Iversen <ianiversen1@gmail.com>
2022-11-21 14:08:04 -0500

    local storage done

--- Browser Storage/script.js
+++ Browser Storage/script.js
@@ -2,10 +2,21 @@
 // document.cookie = 'course=FrontendExpert';
 // console.log(document.cookie);
 
-const course = document
-    .cookie
-    .split('; ')
-    .find(cookie => cookie.startsWith('course='))
-    .split('=')[1];
+// const course = document
+//     .cookie
+//     .split('; ')
+//     .find(cookie => cookie.startsWith('course='))
+//     .split('=')[1];
 
-console.log(course)
+// console.log(course)
+
+//the web storage API => local storage and session storage
+
+//session storage expires, local storage has to be deleted manually
+
+localStorage.setItem('user', 'Ian');
+localStorage.setItem('course', 'AlgoExpert');
+console.log(localStorage.getItem('user'));
+// localStorage.removeItem('user');
+// localStorage.clear();
+console.log(localStorage.getItem('user'));
