d0ee1f73e4193840c0cb31b7cb708684ad46cd25
Ian Iversen <ianiversen1@gmail.com>
2022-11-21 20:17:53 -0500

    typescript done

--- typescript.ts
+++ typescript.ts
@@ -23,4 +23,56 @@
 //     console.log(x + y)
 // }
 
-// add(1, 2);
+
+
+interface Instructor {
+    name: String;
+    course: String;
+    age?: number;
+}
+
+
+// const conner: Instructor = {
+//     name: 'Conner',
+//     course: 'FrontendExpert',
+//     age: 24,
+// };
+
+// const clement: Instructor = {
+//     name: 'Clement',
+//     course: 'AlgoExpert'
+// };
+
+
+// class AlgoExpertInstructor implements Instructor {
+//     name: String;
+//     course: String;
+
+//     constructor(name: string){
+//         this.name = name;
+//         this.course = 'AlgoExpert'
+//     }
+// }
+
+// const tim = new AlgoExpertInstructor('Tim');
+
+//generics
+
+// const arr: Array<number> = [1, 2, 3];
+
+interface GetterSetter<Key, Value> {
+    set: (key: Key, value: Value) => void;
+    get: (key: Key) => Value;
+}
+
+class StringNumGetterSetter implements GetterSetter<string, number> {
+    map: Map<string, number> = new Map();
+
+    set(key: string, value: number): void {
+        this.map.set(key, value);
+    }
+
+    get(key: string): number {
+        return this.map.get(key);
+    }
+}
