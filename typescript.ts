9ef533e596a43a598dd50b519f3c39df6f3b955a
Ian Iversen <ianiversen1@gmail.com>
2022-11-21 19:51:14 -0500

    objects started

--- typescript.ts
+++ typescript.ts
@@ -1,5 +1,26 @@
-let test: number | string | object = 123;
-test = '456';
-test = {};
-console.log(test);
+// type NumString = number | string;
+// let test: NumString = 123;
+// test = '456';
+// console.log(test);
 
+
+// type State = 'off' | 'on';
+// let test: State = 'off';
+// test = 'on';
+// console.log(test);
+
+
+// enum State {
+//     On = 'on',
+//     Off = 'off'
+// }
+
+// let test: State = State.Off;
+// test = State.On;
+// console.log(test);
+
+// function add(x: number, y: number): void {
+//     console.log(x + y)
+// }
+
+// add(1, 2);
