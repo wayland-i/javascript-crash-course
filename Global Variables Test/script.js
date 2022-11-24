aa43385ef7391b0e4f90956ccbb82122ab56656e
Ian Iversen <ianiversen1@gmail.com>
2022-11-23 22:19:02 -0500

    global scope tests

---
+++ Global Variables Test/script.js
@@ -0,0 +1,40 @@
+// 'use strict'
+
+// function print() {
+//     x = 17;
+//     //this will make 'x' global scope
+// }
+
+// print();
+// console.log(x)
+
+
+// function start() {
+//     for (var i = 0; i < 5; i++) {
+//         if (true) {
+//             var color = 'red';
+//         }
+//     }
+//     console.log(color);
+// }
+
+// start();
+
+
+// var color = 'blue'
+
+// function sayHi() {
+//     console.log('hi');
+// }
+
+(function sayHey() {
+    console.log('hey')
+})();
+
+function sayHey() {
+    console.log('hey')
+}
+
+
+
+
